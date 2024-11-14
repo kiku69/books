33011427701e4f2fcf0d978ca281d308a0b0ec02
Kristofer <145542649+kiku69@users.noreply.github.com>
2024-11-14 08:39:19 +0200

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,33 +1,11 @@
+
 <?php
 
 require_once('./connection.php');
 
-$id = $_GET['id'];
+$id = $_POST['id'];
 
-if ( isset($_POST['action']) && $_POST['action'] == 'Kustuta' ){
-
-    $id = $_POST['id'];
-
-    $stmt = $pdo->prepare('UPDATE books SET is_deleted = 1 WHERE id = :id');
-    $stmt->execute(['id' => $id]);
-
-    header("Location: ./book.php?id={ $id }");
-};
-
-$stmt = $pdo->prepare('SELECT * FROM books WHERE id = :id');
+$stmt = $pdo->prepare('UPDATE books SET is_deleted = 1 WHERE id = :id');
 $stmt->execute(['id' => $id]);
-$book = $stmt->fetch();
 
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+header('Location: ./index.php');
